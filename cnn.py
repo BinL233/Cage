0da6fd8d452bd9e1cc3cc1ef5d42d880c5cf6c4a
KaileYing <835741923@qq.com>
2023-10-05 19:16:52 -0400

    Use numpy and sklearn

--- cnn.py
+++ cnn.py
@@ -6,24 +6,34 @@
 
 import torch
 import torch.nn as nn
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
 
-# In[5]:
+# In[2]:
 
 
-raw_input = torch.randint(0, 4, (1, 1000, 1))
-f_input = nn.functional.one_hot(raw_input.squeeze(-1).long(), num_classes=4)
-f_input = f_input.permute(0, 2, 1).float()
+# Create random values
+raw_input = np.random.randint(0, 4, size=(1000, 1))
+
+# One-hot
+encoder = OneHotEncoder(categories='auto', sparse_output=False)
+f_input = encoder.fit_transform(raw_input)
+
+# Convert np matrix to torch matrix
+f_input = torch.tensor(f_input, dtype=torch.float32).unsqueeze(0).permute(0, 2, 1)
 print(f_input.shape)
+print(f_input)
 
 
-# In[6]:
+# In[3]:
 
 
 class SignalCNN(nn.Module):
     def __init__(self):
         super(SignalCNN, self).__init__()
-
+        
+        # layers
         self.conv1 = nn.Conv1d(in_channels=4, out_channels=16, kernel_size=25, stride=1, padding=12)
         self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv1d(in_channels=32, out_channels=1, kernel_size=1)
@@ -41,12 +51,13 @@ class SignalCNN(nn.Module):
 
 
 
-# In[8]:
+# In[5]:
 
 
 model = SignalCNN()
 output = model(f_input)
 print(output.shape)
+print(output)
 
 
 # In[ ]:
