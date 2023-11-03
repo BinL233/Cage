10ceea46ec3f867e5a7174e4e29c759a1090da5a
BinL233 <835741923@qq.com>
2023-11-03 12:51:38 -0400

    Modified file path

--- cage/bigwig_test.py
+++ cage/bigwig_test.py
@@ -1,5 +1,5 @@
 import pyBigWig
 
-bw = pyBigWig.open('../../Data/bpNetlite_ex/hg38.fa')
+bw = pyBigWig.open('../../Data/bpNetlite_ex/ENCSR000AKO_plus.bigWig')
 values = bw.values('chr2',  203781867, 203783123)
 print(values)
